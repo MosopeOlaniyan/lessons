8571ec42f4c2c93bf0e6b576bb60ed18536e592a
Mosope <moshiola14@gmail.com>
2024-07-31 10:44:37 +0100

    Initial Commit

---
+++ lessons/lessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  lessonsApp.swift
+//  lessons
+//
+//  Created by Scholar on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct lessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
